2656732ce49e9a9e28e3f131930cf8024a26bc53
Noah_Sanders <lifegloryhonor@gamil.com>
2020-10-07 10:15:17 -0600

    adds OverlayScrollbars integration + some css

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,18 +1,26 @@
 console.log('script loaded');
-
-const bg = document.getElementById('gContainer');
 let vWidth;
 let vHeight;
+
+document.addEventListener("DOMContentLoaded", function() {
+    OverlayScrollbars(document.getElementById('html'), { 
+    className       : "os-theme-dark theme-container-content",
+    sizeAutoCapable : true,
+    paddingAbsolute : true, 
+    });
+});
+
+const bg = document.getElementById('gContainer');
 
 const bg_resize = function () {
     vWidth = window.innerWidth;
     vHeight = window.innerHeight;
     if (vWidth > (vHeight*1.776)) {
-        bg.style.width = ((vWidth-16.5)+'px');
+        bg.style.width = ((vWidth)+'px');
         bg.style.height = ((vWidth*.562)+'px');
     } else {
         bg.style.width = ((vHeight*1.776)+'px');
-        bg.style.height = ((vHeight-16.5)+'px');
+        bg.style.height = ((vHeight)+'px');
     }
 }
 
